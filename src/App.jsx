9d2ce052c988624d488311add8b19d7b11810a5e
keitey <keiteybarros@gmail.com>
2022-05-25 21:51:07 -0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Card from "./components/Card";
+import { Container } from "./components/styles/Container.styled";
+import { ThemeProvider } from "styled-components";
+import GlobalStyles from "./components/styles/Global";
+import content from "./content";
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+
+  mobile: "768px",
+};
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <>
+      <GlobalStyles />
+        <Header />
+        <Container>
+          {content.map((item, index)=>(
+            <Card key={index} item={item}/>
+          ))}
+        </Container>
+        <Footer />
+      </>
+    </ThemeProvider>
+  );
+};
+
+export default App;
